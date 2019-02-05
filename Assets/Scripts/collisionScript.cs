6576eff21efbf794a3dcff97a7f2413933e1b458
Jacob Turner <me@jacobturner.me>
2019-02-05 09:06:37 -0600

    Initial upload

---
+++ Assets/Scripts/collisionScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour
+{
+    public GameObject FifteenBumperOneLight, FifteenBumperTwoLight, FiftyBumperLight, LeftFlipper, RightFlipper;
+    public int points;
+    public Rigidbody Sphere;
+    public TextMesh PointsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sphere = GetComponent<Rigidbody>();
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "15Bumper1")
+        {
+            points += 15;
+            PointsText.text = "Points: " + points.ToString();
+            FifteenBumperOneLight.SetActive(true);
+        } else if (collision.collider.name == "15Bumper2")
+        {
+            points += 15;
+            PointsText.text = "Points: " + points.ToString();
+            FifteenBumperTwoLight.SetActive(true);
+        } else if (collision.collider.name == "50Bumper")
+        {
+            points += 50;
+            PointsText.text = "Points: " + points.ToString();
+            FiftyBumperLight.SetActive(true);
+        } else if (collision.collider.tag == "Flipper" && Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            Sphere.AddForce(0f, 0f, 200f);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.name == "15Bumper1")
+        {
+            FifteenBumperOneLight.SetActive(false);
+        }
+        else if (collision.collider.name == "15Bumper2")
+        {
+            FifteenBumperTwoLight.SetActive(false);
+        }
+        else if (collision.collider.name == "50Bumper")
+        {
+            FiftyBumperLight.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/flipperScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipperScript : MonoBehaviour
+{
+    public GameObject LeftFlipper, RightFlipper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LeftFlipper.transform.Rotate(0f, 0f, 33f);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            LeftFlipper.transform.Rotate(0f, 0f, -33f);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RightFlipper.transform.Rotate(0f, 0f, -33f);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            RightFlipper.transform.Rotate(0f, 0f, 33f);
+        }
+    }
+}
